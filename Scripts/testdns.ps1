49a5ceecc6535035ea7c8ac667d7857485c3e7f1
Shawn Ayotte <sayotte@ekota.ca>
2019-08-15 15:37:20 -0600

    Add files via upload

--- Scripts/testdns.ps1
+++ Scripts/testdns.ps1
@@ -1,24 +1,22 @@
 $numberoftests = 10
-	$Networks = Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE -ErrorAction Stop
-	foreach($Network in $Networks) {
-	  $DNSServers = $Network.DNSServerSearchOrder
+$Networks = Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE -ErrorAction Stop
+foreach($Network in $Networks) {
+  $DNSServers = $Network.DNSServerSearchOrder
+}
+$i = 0
+Write-Host "DNS Servers assigned to this host: $DNSServers"
+$DNSServers += "1.1.1.1"
+$DNSServers += "8.8.4.4"
+foreach ($dnsserver in $DNSServers){
+	# $totalmeasurement = 0
+	while ($i -ne $numberoftests)
+	{
+		$measurement = (Measure-Command {Resolve-DnsName www.bing.com -Server $dnsserver –Type A}).TotalSeconds
+		$totalmeasurement += $measurement
+		$i += 1
 	}
-	$totalmeasurement = 0
-	$i = 0
-	Write-Host "DNS Servers assigned to this host: $DNSServers"
-	$DNSServers += "1.1.1.1"
-	$DNSServers += "8.8.4.4"
-	foreach ($dnsserver in $DNSServers){
-		while ($i -ne $numberoftests)
-		{
-			$measurement = (Measure-Command {Resolve-DnsName www.bing.com -Server $dnsserver –Type A}).TotalSeconds
-			$totalmeasurement += $measurement
-			$i += 1
-		}
-		$totalmeasurement = $totalmeasurement / $numberoftests
-		write-host "DNS Server:  $dnsserver, Response time: $totalmeasurement seconds"
-		$Networks = Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE -ErrorAction Stop
-		foreach($Network in $Networks) {
-		  $DNSServers = $Network.DNSServerSearchOrder
-		}
-	}
+	$totalmeasurement = $totalmeasurement / $numberoftests
+	$totalmeasurement = [math]::Round(($totalmeasurement),4)
+	write-host "DNS Server:  $dnsserver, Response time: $totalmeasurement seconds"
+	
+}
